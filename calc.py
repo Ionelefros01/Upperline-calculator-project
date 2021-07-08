e907ae12f4f406db322c86393133b037c577e061
Ion Efros <ionel.efros01@gmail.com>
2021-07-08 18:32:59 +0000

    The final version of the calculator

--- calc.py
+++ calc.py
@@ -4,13 +4,21 @@
 
 budget = input("What is your net monthly salary? ")
 essential_spendings = input("How much money do you spend on living expenses? ")
+# save_account = input("How much money do you want to send to the saving account? ")
 vacation_expenses = input("How much money does your vacation requires? ")
-stress_level = input("On a scale of 1 to 10 how stressfull you felt this year? ") #Done
+stress_level = input("On a scale of 1 to 10 how stressfull you felt this year? ") 
+
 
 # Calculate the saved money:
 
 money_saved = (int(budget) - int(essential_spendings)) * 12
 
+print(f"\nFor the past year you saved ${money_saved}!")
+
+save_account = input("How much money do you want to send to the saving account? ")
+
+
+money_you_can_spend = ((int(budget) - int(essential_spendings))  * 12 - int(save_account))
 
 # Function for stress level
 
@@ -24,10 +32,12 @@ print("\n" + vacation_stress_status(stress_level))
 
 print(f"\nFor the past year you saved ${money_saved}!")
 
+print(f"\nBecause you want to send ${save_account} to your saving account, you can spend just ${money_you_can_spend}")
+
 def enough_money_for_vacation(money):
-    if int(money_saved) >= int(vacation_expenses):
+    if int(money_you_can_spend) >= int(vacation_expenses):
         return (f"Your vacation cost ${vacation_expenses}, so you can afford it. Have a great vacation!")
-    if int(money_saved) < int(vacation_expenses):
+    if int(money_you_can_spend) < int(vacation_expenses):
         return (f"Your vacation cost ${vacation_expenses}, so you can't afford it. Please save more money and try to go on a vacation next year.")
 
 print ("\n" + enough_money_for_vacation(vacation_expenses) + "\n")
